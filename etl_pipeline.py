d3107d0d549857de1dd8534df2b5e16fc71dc5df
Kristiina Marie Palu <krpalh@taltech.ee>
2026-02-28 20:37:16 +0200

    extract csv with pandas

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,2 +1,15 @@
 import pandas as pd
 
+def extract_data_from_csv(file):
+    """
+    Extract and return the data in DataFrame format from the csv file, using pandas.
+    :param file:
+    :return: data
+    """
+    data = pd.read_csv(file)
+    return data
+
+
+#Testing
+data = extract_data_from_csv("csv_files/eurofxref.csv")
+print(data)
