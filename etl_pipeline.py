cce116f80afef1a8354fa6b5017fcfbf1470845a
Kristiina Marie Palu <krpalh@taltech.ee>
2026-02-28 21:47:04 +0200

    Fixed the double currency codes

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -5,9 +5,11 @@ CURRENCY_CODES = ["USD", "SEK", "GBP", "JPY"]
 def extract_data_from_csv(daily_rates_file, historical_rates_file):
     """
     Extract and return the data in DataFrame format from the csv file, using pandas.
-    :param file:
+    :param historical_rates_file:
+    :param daily_rates_file:
     :return: data
     """
+    #Read the CSV file to a DataFrame format
     daily_data = pd.read_csv(daily_rates_file, skipinitialspace=True)
     historical_data = pd.read_csv(historical_rates_file, skipinitialspace=True)
 
@@ -20,7 +22,7 @@ def transform(daily_data, historical_data):
     :param historical_data:
     :return: mean_average, transformed_daily_rates
     """
-    transformed_daily_rates = daily_data[CURRENCY_CODES]
+    transformed_daily_rates = daily_data[CURRENCY_CODES].iloc[0] # To a series (1 dimensional)
     transformed_historical_rates = historical_data[CURRENCY_CODES]
 
     mean_average = transformed_historical_rates.mean(skipna=True)
@@ -33,8 +35,9 @@ def load_data(mean_average, transformed_daily_rates):
     :param transformed_daily_rates:
     :return: md_table
     """
-    md_table = pd.DataFrame({"CURRENCY CODES": CURRENCY_CODES,
-                   "MEAN_AVERAGE": mean_average})
+    md_table = pd.DataFrame({"Currency code": CURRENCY_CODES,
+                             "Rate": transformed_daily_rates.values,
+                             "Mean historical rate": mean_average.values})
     return md_table
 
 def run_etl_pipeline():
