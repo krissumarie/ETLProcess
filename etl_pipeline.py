40e4d78de289b875f91c87ca0de4b7d2868babac
Kristiina Marie Palu <krpalh@taltech.ee>
2026-02-28 21:08:24 +0200

    Added the historical csv extract and fixed the initial space problem on daily rate columns

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,24 +1,27 @@
 import pandas as pd
 
-def extract_data_from_csv(file):
+def extract_data_from_csv(daily_rates, historical_rates):
     """
     Extract and return the data in DataFrame format from the csv file, using pandas.
     :param file:
     :return: data
     """
-    data = pd.read_csv(file)
-    return data
+    daily_data = pd.read_csv(daily_rates, skipinitialspace=True)
+    historical_data = pd.read_csv(historical_rates, skipinitialspace=True)
+
+    return daily_data, historical_data
 
 def transform(daily_rates, historical_rates):
 
-    transformed_daily_rates = daily_rates[[" USD", " SEK", " GBP", " JPY"]]
+    transformed_daily_rates = daily_rates[["USD", "SEK", "GBP", "JPY"]]
+    transformed_historical_rates = historical_rates[["USD", "SEK", "GBP", "JPY"]]
+
     return transformed_daily_rates
 
 
 def run_etl_pipeline():
-    data = extract_data_from_csv("csv_files/eurofxref.csv")
-    transformed_data = transform(data)
-    print(data)
+    daily, historical = extract_data_from_csv("csv_files/eurofxref.csv", "csv_files/eurofxref-hist.csv")
+    transformed_data = transform(daily, historical)
     print(transformed_data)
 
 run_etl_pipeline()
