2bb86adb4cfbe8239cf27ebf03ef21701fbd6e19
Kristiina Marie Palu <krpalh@taltech.ee>
2026-03-01 15:00:20 +0200

    Changed some variable names and cleaned up the code. Changed the safety for the URL reading in the etl pipeline process

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -4,6 +4,7 @@ CURRENCY_CODES = ["USD", "SEK", "GBP", "JPY"]
 DAILY_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip"
 HISTORICAL_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip"
 
+
 def extract_csv_from_zip(url):
     """
     Try to extract csv file.
@@ -11,46 +12,54 @@ def extract_csv_from_zip(url):
     """
     return pd.read_csv(url, compression="zip", skipinitialspace=True)
 
+
 def transform(daily_data, historical_data):
     """
     Select only necessary currency codes, calculate mean historical rates.
     :param daily_data:
     :param historical_data:
-    :return: mean_average, transformed_daily_rates
+    :return: mean_rates, transformed_daily_rates
     """
     transformed_daily_rates = daily_data[CURRENCY_CODES].iloc[0] # To a Series (1 dimensional)
     transformed_historical_rates = historical_data[CURRENCY_CODES]
 
-    mean_average = transformed_historical_rates.mean(skipna=True)
-    return mean_average, transformed_daily_rates
+    mean_rates = transformed_historical_rates.mean(skipna=True)
+    return mean_rates, transformed_daily_rates
 
-def load_data(mean_average, transformed_daily_rates):
+
+def load_data(mean_rates, transformed_daily_rates):
     """
     Create a Markdown table and save it into a file.
-    :param mean_average:
+    :param mean_rates:
     :param transformed_daily_rates:
     :return: md_table
     """
-    md_table = pd.DataFrame({"Currency code": transformed_daily_rates.index,
-                             "Rate": transformed_daily_rates.values,
-                             "Mean historical rate": mean_average.values})
+    md_table = pd.DataFrame(
+        {
+            "Currency code": transformed_daily_rates.index,
+            "Rate": transformed_daily_rates.values,
+            "Mean historical rate": mean_rates.values
+        }
+    )
 
     md_table.to_markdown("exchange_rates.md", index=False)
     return md_table
 
+
 def run_etl_pipeline():
     """
-    Run the ETL process
+    Run the ETL process. First read csv from the url using pandas, then transform the csv files and
+    calculate the mean historical rates. Then load the data to a markdown file.
     """
-    daily_df = extract_csv_from_zip(DAILY_URL)
-    historical_df = extract_csv_from_zip(HISTORICAL_URL)
-    if daily_df is None or historical_df is None:
-        print("ETL pipeline failed")
-        return
+    try:
+        daily_df = extract_csv_from_zip(DAILY_URL)
+        historical_df = extract_csv_from_zip(HISTORICAL_URL)
 
-    mean_average, daily_transformed_data = transform(daily_df, historical_df)
-    load_data(mean_average, daily_transformed_data)
-    print("ETL pipeline succeeded")
+        mean_rates, daily_transformed_data = transform(daily_df, historical_df)
+        load_data(mean_rates, daily_transformed_data)
+        print("ETL pipeline succeeded")
+    except Exception as e:
+        print(f"ETL pipeline failed: {e}")
 
 
 if __name__ == "__main__":
